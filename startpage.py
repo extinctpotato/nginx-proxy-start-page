dc6a519a09f983ba85f87b636122f7687aeef1e8
Adam Olech <nddr89@gmail.com>
2019-09-25 23:35:33 +0200

    Decoration parsing

--- startpage.py
+++ startpage.py
@@ -1,6 +1,12 @@
 from nginxparser_eb import load
 import os
 
+def spsplit(line):
+    allowed = ("sp-title", "sp-desc", "sp-icon")
+    line_split = line.split(" ", 1)
+    if line_split[0] in allowed:
+        return {line_split[0] : line_split[1].replace("\"", "")}
+    
 class NGINX:
     def __init__(self, path):
         self.parsed = load(open(path))
@@ -8,9 +14,15 @@ class NGINX:
         sift = []
         for i in range(len(self.parsed[0][1])):
             element_for_eval = self.parsed[0][1][i]
+            block_dict = {}
 
             if element_for_eval[0][0] == "location":
                 for j in range(len(element_for_eval[1])):
+                    if element_for_eval[1][j][0] == "#":
+                        decoration = spsplit(element_for_eval[1][j][1])
+                        if decoration:
+                            block_dict.update(decoration)
                     if element_for_eval[1][j][0] == "proxy_pass":
-                        sift.append(element_for_eval[0][1])
+                        block_dict.update({"location":element_for_eval[0][1]})
+                        sift.append(block_dict)
         return sift
